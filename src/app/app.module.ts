d97cdc529b5fac73e2bfd24ab13e2833ac63df5e
MichalBoczula <michal.boczula0101@gmail.com>
2021-06-19 00:35:35 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesListItemComponent } from './recipes/recipes-list/recipes-list-item/recipes-list-item.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesListItemComponent,
+    RecipesDetailsComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
